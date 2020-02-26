fa84856ba3d3653fa723ceb878d77b30982c2fc3
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-02-26 17:11:33 -0500

    updated schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Town Topic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kimchi Burger');
+INSERT INTO burgers (burger_name) VALUES ('Partty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Royale Burger');
+INSERT INTO burgers (burger_name) VALUES ('Le Big Mac');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('White Castle Slider', true);
